9ae78374664e57a5bd01baf246cec0732581c43f
Suzanne Rozier <srozier@squareup.com>
2025-02-18 17:15:23 -0600

    Strip out stuff

--- apps/my-app/src/App.tsx
+++ apps/my-app/src/App.tsx
@@ -1,20 +1,11 @@
-import { useState } from 'react'
 import { MyComponent, MyOtherComponent } from 'my-component-library'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+ return (
     <>
       <MyComponent />
       <MyOtherComponent />
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
     </>
   )
 }
